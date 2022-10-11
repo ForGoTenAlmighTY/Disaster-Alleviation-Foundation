8ae5533f8df18194588a622926cc9f993bb8eb7f
Victor Leping <ST10127626@rcconnect.edu.za>
2022-10-12 01:19:01 +0200

    Updated LogInPage

--- Disaster Alleviation Foundation/LogIn.aspx.cs
+++ Disaster Alleviation Foundation/LogIn.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,44 @@ namespace Disaster_Alleviation_Foundation
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            //CONNECTION TO THE DATABASE
+            SqlConnection con = new SqlConnection(@"Server=tcp:disasterfoundation.database.windows.net,1433;Initial Catalog=DisasterAlleviationDatabase;Persist Security Info=False;User ID=DisasterAlleviation;Password=Vikitar15!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM USERS WHERE Email='" + txtEmail.Text + "' AND Password ='" + txtPassword.Text + "'", con);
+            con.Open();
+
+            //EXECUTES THE QUERY DECLARED
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+
+            bool check = false;
+
+            //ASSIGNS A SESSION NUMBER TO THE USER WHO HAS LOGIN OR REGISTERED
+
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                check = true;
+
+            }
+
+
+            //CHECKS IF THE USER ENTERS THE CORRECT DETAIS TO LOGIN
+            if (check == true)
+            {
+                Response.Redirect("~/Home.aspx");
+            }
+            else
+            {
+                Response.Redirect("~/LogIn.aspx");
+            }
+
+            con.Close();
         }
     }
 }
