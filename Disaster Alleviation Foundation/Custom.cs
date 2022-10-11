9d79752c31479aa466fabb08f33e85c08501f1b5
Victor Leping <ST10127626@rcconnect.edu.za>
2022-10-12 01:12:56 +0200

    Add project files.

---
+++ Disaster Alleviation Foundation/Custom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Disaster_Alleviation_Foundation
+{
+    public class Custom
+    {
+        private int sessionID=0;
+
+        public int getSessionID()
+        {
+            return sessionID;
+        }
+
+        public void setSessionID(int value)
+        {
+            sessionID = value;
+        }
+    }
+}
