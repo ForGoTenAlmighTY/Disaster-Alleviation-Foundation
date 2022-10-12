a24b341f1e73b0d384bade1185dd30057538f946
Victor Leping <ST10127626@rcconnect.edu.za>
2022-10-12 19:17:51 +0200

    Added and edited GoodsAdmin page

---
+++ Disaster Alleviation Foundation/GoodsAdmin.aspx.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Disaster_Alleviation_Foundation
+{
+    public partial class GoodsAdmin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddCategory_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Server=tcp:disasterfoundation.database.windows.net,1433;Initial Catalog=DisasterAlleviationDatabase;Persist Security Info=False;User ID=DisasterAlleviation;Password=Vikitar15!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+            //INSERT QUERY TO THE DATABASE
+            SqlCommand sqlCommand = new SqlCommand("INSERT INTO Item VALUES ('" + txtName.Text + "','" + txtPrice.Text + ")", con);
+            con.Open();
+
+
+
+            SqlDataReader dataReader = sqlCommand.ExecuteReader();
+            SqlDataReader reader = dataReader;
+
+
+
+            con.Close();
+            Response.Redirect("~/HomeAdmin.aspx");
+
+        }
+    }
+}
