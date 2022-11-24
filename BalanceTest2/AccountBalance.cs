2c50a8d97857c64508ab561752d417f001f1a0b1
Victor Leping <ST10127626@rcconnect.edu.za>
2022-11-24 02:36:08 +0200

    Added Test method to check balance of users

--- BalanceTest2/AccountBalance.cs
+++ BalanceTest2/AccountBalance.cs
@@ -6,9 +6,27 @@ namespace BalanceTest2
     [TestClass]
     public class AccountBalance
     {
+        
         [TestMethod]
-        public void TestMethod1()
+        public void Donated_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
         {
+            // Arrange
+            double beginningBalance = 50000;
+            double DonatedAmount = -10000.00;
+            Custom account = new Custom("Red Cross", beginningBalance);
+
+            try
+            {
+                account.Debit(DonatedAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, Custom.DonatedAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
         }
     }
 }
